1ccc3e4b6d3142bd67a50cf5b74679ac92b894e8
Sergey <ratuhniy.93@yandex.ru>
2022-10-29 00:27:58 +0300

    Сделал 12 задачу + FixBugs + Chenge README

--- work010/Program.cs
+++ work010/Program.cs
@@ -6,7 +6,7 @@ int number = Convert.ToInt32(Console.ReadLine());                               
 System.Console.WriteLine();                                                                     // Пустая строка
 
 if (1000 > number && number > 99)                                                               // Если number меньше 1000 и больше 99
-    System.Console.WriteLine($"Последняя цифра чила {number} является: {number / 10 % 10} ");   // Вывод (остаток от деления на 10 после деления на 10, целочисленное)
+    System.Console.WriteLine($"Вторая цифра чила {number} является: {number / 10 % 10} ");   // Вывод (остаток от деления на 10 после деления на 10, целочисленное)
 else                                                                                            // Иначе
 {
     System.Console.WriteLine("Начните заново и Введите корректное число");                      // Вывод
--- work012/Program.cs
+++ work012/Program.cs
@@ -1,2 +1,21 @@
 ﻿// 12. Удалить вторую цифру трёхзначного числа
 
+System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());                                                        // Ввод
+
+System.Console.WriteLine();                                                                              // Пустая строка
+
+int newNumber = 0;
+
+if (999 > number && number > 99)                                                                         // Если number меньше 1000 и больше 99
+    
+    {
+    newNumber = number / 100 * 10 + number % 10;                                                        // Убираем вторую цифру
+    System.Console.WriteLine($"Если из числа {number} удалить вторую цифру, получится число {newNumber}");  // Вывод
+    }
+else                                                                                                     // Иначе
+{
+    System.Console.WriteLine("Начните заново и Введите корректное число");                               // Вывод
+}
+
+ 
