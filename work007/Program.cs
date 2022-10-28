1e02ebe63f365927578b09cec56ad4b02be17109
Sergey <ratuhniy.93@yandex.ru>
2022-10-28 16:50:28 +0300

    fixBugs

--- work007/Program.cs
+++ work007/Program.cs
@@ -1,6 +1,7 @@
 ﻿// 7. Показать числа от -N до N
 Console.WriteLine("Введите число");
-string writeNumber = Console.ReadLine();
+string? writeNumber = Console.ReadLine();
+System.Console.WriteLine();
 int endNumber = Convert.ToInt32(writeNumber);
 int Number = endNumber * (-1);
 System.Console.WriteLine(Number);
