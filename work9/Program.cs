84231681c878be4cdaa9531c58ae6b1103758a16
Sergey <ratuhniy.93@yandex.ru>
2022-10-28 16:06:37 +0300

    Сделал задание 9

---
+++ work9/Program.cs
@@ -0,0 +1,12 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа 
+
+System.Console.WriteLine("Введите трехзначное число");                                      // Ввод
+int number = Convert.ToInt32(Console.ReadLine());                                           // присваиваем
+System.Console.WriteLine();                                                                 // Пустая строка
+
+if (1000 > number && number > 99)                                                           // Если number меньше 1000 и больше 99
+    System.Console.WriteLine($"Последняя цифра чила {number} является: {number % 10} ");    // Вывод (остаток от деления на 10)
+else                                                                                        // Иначе
+{
+    System.Console.WriteLine("Начните заново и Введите корректное число");                  // Вывод
+}
