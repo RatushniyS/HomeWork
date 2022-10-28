f0f349bb458cb4d8adcaeadfc8f6e654550f50d8
Sergey <ratuhniy.93@yandex.ru>
2022-10-28 16:42:06 +0300

    Решил задачку 10 11

---
+++ work10/Program.cs
@@ -0,0 +1,13 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+System.Console.WriteLine("Введите трехзначное число");                                          // Ввод
+int number = Convert.ToInt32(Console.ReadLine());                                               // присваиваем
+
+System.Console.WriteLine();                                                                     // Пустая строка
+
+if (1000 > number && number > 99)                                                               // Если number меньше 1000 и больше 99
+    System.Console.WriteLine($"Последняя цифра чила {number} является: {number / 10 % 10} ");   // Вывод (остаток от деления на 10 после деления на 10, целочисленное)
+else                                                                                            // Иначе
+{
+    System.Console.WriteLine("Начните заново и Введите корректное число");                      // Вывод
+}
---
+++ work11/Program.cs
@@ -0,0 +1,16 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int number = new Random().Next(10, 99);                                                             // присваеваем к number случайное целое число от 10 до 99
+System.Console.WriteLine($"Число {number}");                                                        // вывод
+System.Console.WriteLine();                                                                         // пустая строка
+
+int FirstDigit = number / 10;                                                                       // поделив на 10 получим первую цифру (целое)
+int SecondDigit = number % 10;                                                                      // остаток от деления
+
+if (FirstDigit > SecondDigit)                                                                       // Если
+
+    System.Console.WriteLine($"В числе {number} наибольшей цифврой является {FirstDigit}");         // Вывод
+
+else
+{
+    System.Console.WriteLine($"В числе {number} наибольшей цифврой является {SecondDigit}");        // Вывод
+}
