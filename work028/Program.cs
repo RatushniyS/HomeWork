fa64d7d1dac879a4794c17633bf11253d1d50f2f
Sergey <ratuhniy.93@yandex.ru>
2022-11-03 22:48:45 +0300

    Completed the task HomeWork 29 30 & bugFix

--- work028/Program.cs
+++ work028/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Подсчитать сумму цифр в числе
+
+Console.Clear();
+System.Console.WriteLine();
+
+System.Console.WriteLine("Введите Число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+System.Console.Write($"Сумма всех чисел числа {number} = ");
+
+for (int i = 0; i <= number; i++)
+{
+    int digit = number % 10;
+    if (digit == 1)
+        sum = sum + 1;
+    else
+    sum = sum + digit;
+    number = number / 10;
+}
+
+
+System.Console.WriteLine(sum);
---
+++ work029/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+// Зная функцию лонг могу посчитать только до N = 20
+// Цифры больше ещё не изучил
+
+Console.Clear();
+System.Console.WriteLine();
+
+System.Console.WriteLine("Введите Число: ");
+long N = Convert.ToInt64(Console.ReadLine());
+
+
+long CompositionNumbers(long i)
+{
+    long composition = 1;
+    for (long num = 1; num <= i; num++)
+    {
+        composition = composition * num;
+    }
+    return composition;
+}
+
+System.Console.WriteLine($"Произведение чисел от 1 до {N} равно {CompositionNumbers(N)}");
+System.Console.WriteLine();
