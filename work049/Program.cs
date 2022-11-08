a775f1e9e0f50fd0fdea6fbb653ccaddbb961095
Sergey <ratuhniy.93@yandex.ru>
2022-11-08 14:04:45 +0300

    Completed the task HomeWork 49 & bugFix

---
+++ work049/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив размером i×j, заполненный случайными вещественными числами.
+
+int Input(string variable)
+{
+    Console.WriteLine($"Введите {variable}");
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-30, 30));    // Можно поделить на 10, чтоб наглядно показать что числа вещественные
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+int i = Input("количество строк");
+int j = Input("количество столбцов");
+double[,] matrix = new double[i, j];
+FillArray(matrix);
+PrintArray(matrix);
---
+++ workSem6/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте двумерный массив. 
+// Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки 
+// двумерного массива
+
+
+void FillArray(int[,] array)
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+int[,] BubbleSort(int[,] array)
+{
+    int temp;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] > array[i, k])
+                {
+                    temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int Input(string variable)
+{
+    Console.WriteLine($"Введите {variable}");
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+
+int i = Input("количество строк");
+int j = Input("количество столбцов");
+int[,] matrix = new int[i, j];
+FillArray(matrix);
+BubbleSort(matrix);
+PrintArray(matrix);
