256126dad206d0b391a82169b54eabfc797b4f0b
Sergey <ratuhniy.93@yandex.ru>
2022-11-11 00:18:21 +0300

    Completed the task HomeWork 63 65 & bugFix

---
+++ bonuswork01/Program.cs
@@ -0,0 +1,63 @@
+﻿System.Console.WriteLine("Введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine();
+
+int[,] desk = new int[n, m];
+
+// int horse = desk[0, 0];             // Конь на позиции 0 0
+
+int[][] step =
+    {
+        new int[] {2, 1},
+        new int[] {1, 2},
+        // new int[] {1, -2},              // Куда может шагнуть конь
+        // new int[] {2, -1},              // Многомерный массив
+        // new int[] {-1, 2},
+        // new int[] {2, 1},
+        // new int[] {-2, -1},
+        // new int[] {-1, -2},
+
+    };
+
+
+void SetHorse(int x, int y)
+{
+    desk[x, y] = 0;
+    for (int i = 0; i < 2; i++)
+    {
+        desk[x + step[i][0], y + step[i][1]] = i + 1;
+    }
+}
+
+
+
+
+void PrintDesk(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{desk[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void Main(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            desk[i, j] = 0;
+        }
+    }
+    SetHorse(0, 0);
+    PrintDesk(desk);
+}
+
+Main(desk);
--- work065/Program.cs
+++ work065/Program.cs
@@ -2,9 +2,8 @@
 
 System.Console.WriteLine("Введите количество строк/столбцов");
 int n = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine(
-  
-);
+System.Console.WriteLine();
+
 int[,] Matrix = new int[n, n];
 
 int temp = 1;
@@ -13,31 +12,31 @@ int j = 0;
 
 while (temp <= Matrix.GetLength(0) * Matrix.GetLength(1))
 {
-  Matrix[i, j] = temp;
-  temp++;
-  if (i <= j + 1 && i + j < Matrix.GetLength(1) - 1)
-    j++;
-  else if (i < j && i + j >= Matrix.GetLength(0) - 1)
-    i++;
-  else if (i >= j && i + j > Matrix.GetLength(1) - 1)
-    j--;
-  else
-    i--;
+    Matrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < Matrix.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= Matrix.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > Matrix.GetLength(1) - 1)
+        j--;
+    else
+        i--;
 }
 
 WriteArray(Matrix);
 
-void WriteArray (int[,] array)
+void WriteArray(int[,] array)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      if (array[i,j] / 10 <= 0)
-      Console.Write($" {array[i,j]} ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+                Console.Write($" {array[i, j]} ");
 
-      else Console.Write($"{array[i,j]} ");
+            else Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
-  }
 }
