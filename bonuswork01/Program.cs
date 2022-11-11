80071a360bbd8984f9aa28761a961b035fb7530e
Sergey <ratuhniy.93@yandex.ru>
2022-11-11 18:16:18 +0300

    Completed the task HomeWork BonusWork & bugFix

--- bonuswork01/Program.cs
+++ bonuswork01/Program.cs
@@ -6,34 +6,48 @@ int m = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine();
 
 int[,] board = new int[n, m];
-
-// int horse = board[0, 0];             // Конь на позиции 0 0
-
 int[][] step =
     {
-        new int[] {2, 1},
         new int[] {1, 2},
-        // new int[] {1, -2},              // Куда может шагнуть конь
-        // new int[] {2, -1},              // Многомерный массив
-        // new int[] {-1, 2},
-        // new int[] {2, 1},
-        // new int[] {-2, -1},
-        // new int[] {-1, -2},
-
+        new int[] {1, -2},              // Куда может шагнуть конь
+        new int[] {2, 1},
+        new int[] {2, -1},              // Многомерный массив
+        new int[] {-1, 2},
+        new int[] {-1, -2},
+        new int[] {-2, 1},
+        new int[] {-2, -1},
     };
- 
+int moves;
 
-void SetHorse(int x, int y)
+bool SetHorse(int x, int y)
 {
-    board[x, y] = 0;
-    for (int i = 0; i < 2; i++)
+    // Проверка параметров
+    if ((x < 0) || (x >= n) || (y < 0) || (y >= n))
+        return false;
+
+    if (board[x, y] != 0)
+        return false;
+
+    // Остановка
+    if (moves == n * m - 1)
+        return true;
+    // следующий ход
+    moves++;
+    board[x, y] = moves;
+    for (int i = 0; i < 8; i++)
     {
-        board[x + step[i][0], y + step[i][1]] = i + 1;
+        if (SetHorse(x + step[i][0], y + step[i][1]))
+            return true;
+
+        // возврат хода
+
+        moves--;
+        board[x, y] = 0;
     }
+
+
+    return false;
 }
-
-
-
 
 void PrintBoard(int[,] array)
 {
@@ -49,6 +63,7 @@ void PrintBoard(int[,] array)
 
 void NoName(int[,] array)
 {
+    moves = 0;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
